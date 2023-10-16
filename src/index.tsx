588d0b067419e0ca7dc1f902d976bcfcf97e7297
sailalith <sailalith.raju@conversight.ai>
2023-10-16 08:41:05 +0530

    extracting the code editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,15 @@
-import  ReactDOM  from "react-dom";
-import { useState, useEffect, useRef } from "react";
-import {createRoot} from 'react-dom/client'
-
-import CodeEditor from "./components/code-editor";
+import { createRoot } from 'react-dom/client'
+import CodeCell from "./components/code-cell";
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import Preview from "./components/preview";
-import bundle from './bundler'
-const App = () =>{
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-  const ref = useRef<any>();
 
-  const onClick = async () =>{
-    const output = await bundle(input);
-    setCode(output);
-    
-    
-  };
- 
-
+const App = () => {
   return <div>
-    <CodeEditor onChange={(value)=>setInput(value)} initialValue={input}/>
-    <div>
-      <button onClick={onClick}>Submit</button>
-    </div>
-      <Preview code={code}  />
-
+    <CodeCell />
   </div>;
 };
 
 const container = document.querySelector('#root');
 const root = createRoot(container!);
 root.render(
-  <App/>
+  <App />
 )
