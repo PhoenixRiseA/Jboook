3f673f6eab8075157b8e5a2d6d34bcd844feebd4
sailalith <sailalith.raju@conversight.ai>
2023-10-16 08:03:55 +0530

    refactorying out a preview component

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ import {createRoot} from 'react-dom/client'
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import Preview from "./components/preview";
 const App = () =>{
   const [input, setInput] = useState('');
-  
+  const [code, setCode] = useState('');
   const ref = useRef<any>();
-  const iframe = useRef<any>();
   const startService = async () =>{
     ref.current = await esbuild.startService({
       worker: true,
@@ -22,7 +22,7 @@ const App = () =>{
     if(!ref.current){
       return;
     }
-    iframe.current.srcdoc = html;
+    
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -37,8 +37,8 @@ const App = () =>{
       }
     });
     // console.log(result);
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    setCode(result.outputFiles[0].text);
+    
     
   };
 
@@ -46,32 +46,15 @@ const App = () =>{
     startService();
   },[]);
 
-  const html = `
-  <html>
-    <head></head>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message',(event)=>{
-        try{
-          eval(event.data);
-        }catch (err){
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
-          console.log(err);
-        }
-        
-      },false)
-    </script>
-  </html>
-`;
+ 
 
   return <div>
     <CodeEditor onChange={(value)=>setInput(value)} initialValue={input}/>
     <div>
       <button onClick={onClick}>Submit</button>
     </div>
+      <Preview code={code}  />
 
-    <iframe title="preview" sandbox="allow-scripts" ref={iframe} srcDoc={html}></iframe>
   </div>;
 };
 
