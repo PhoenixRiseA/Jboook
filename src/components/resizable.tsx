c827ee3048d692411004b38659bea4c69ff953e4
sailalith <sailalith.raju@conversight.ai>
2023-11-08 08:31:56 +0530

    give resizeStop logic for horizontal object

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from 'react';
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
-  children?: React.ReactNode;  
+  children?: React.ReactNode;
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
-  const [innerWidth, setInnerwidth] = useState(window.innerWidth);
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
-  const [width, setWidth] = useState<number>(window.innerWidth*0.75);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
   useEffect(() => {
-    let timer:any;
-    const listener = () =>{
-      if(timer){
+    let timer: any;
+    const listener = () => {
+      if (timer) {
         clearTimeout(timer);
       }
-      timer = setTimeout(()=>{
-        setInnerwidth(window.innerWidth);
+      timer = setTimeout(() => {
         setInnerHeight(window.innerHeight);
-        if(window.innerWidth*75 < width){
-          setWidth(window.innerWidth*75)
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
         }
-      },100)
-     
-
+      }, 100);
     };
-    window.addEventListener('resize',listener)
-  
+    window.addEventListener('resize', listener);
+
     return () => {
-      window.removeEventListener('resize',listener)
-    }
-  }, [])
-  
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
@@ -43,6 +42,9 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       height: Infinity,
       width,
       resizeHandles: ['e'],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
@@ -54,13 +56,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     };
   }
 
-  return <ResizableBox
-   {...resizableProps}
-   onResizeStop={(event,data)=>{
-       
-       setWidth(data.size.width)
-   }}
-   >{children}</ResizableBox>;
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
