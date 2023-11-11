a08cfd7b0c8230322a8ad621dd1a50e81ee9dfd2
sailalith <sailalith.raju@conversight.ai>
2023-11-11 19:56:28 +0530

    creating a markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import CodeCell from "./components/code-cell";
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return <div>
-    <CodeCell />
+    {/* <CodeCell /> */}
+    <TextEditor/>
   </div>;
 };
 
